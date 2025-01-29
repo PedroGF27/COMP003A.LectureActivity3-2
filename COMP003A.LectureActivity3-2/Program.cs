f4f9c8da478b1b697d5aa005b2f1e52a49852033
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-01-29 13:21:30 -0800

    added the code to generate multiplication table for a positive integer

--- COMP003A.LectureActivity3-2/Program.cs
+++ COMP003A.LectureActivity3-2/Program.cs
@@ -8,7 +8,21 @@ namespace COMP003A.LectureActivity3_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Iterative Statements Demo\n");
+
+            int num;
+            Console.Write("Enter a positive integer to generate its multiplication table: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Multiplication Table for {num}:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} x {num} = {i * num}");
+            }
+
+            Console.WriteLine("\nReversed Multiplication Table:");
+            int reverse = 10;
         }
     }
 }
