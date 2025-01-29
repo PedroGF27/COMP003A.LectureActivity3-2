e908ff44d006abaea5fee98036144fc85da8581d
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-01-29 13:34:29 -0800

    added code for reverse multiplication table

--- COMP003A.LectureActivity3-2/Program.cs
+++ COMP003A.LectureActivity3-2/Program.cs
@@ -10,19 +10,38 @@ namespace COMP003A.LectureActivity3_2
         {
             Console.WriteLine("Welcome to the Iterative Statements Demo\n");
 
+            // Prompt to put positive integer
             int num;
             Console.Write("Enter a positive integer to generate its multiplication table: ");
             num = int.Parse(Console.ReadLine());
 
+            // Generate multiplication table for loop
             Console.WriteLine($"Multiplication Table for {num}:");
 
+            // Loop that generates multiplication table
+            // 1st statement: initializes loop variable i to 1
+            // 2nd statement: specifies condition for loop to continue
+            // 3rd statement: increments loop variable i by 1
             for (int i = 1; i <= 10; i++)
             {
+                // shows multiplication table
                 Console.WriteLine($"{i} x {num} = {i * num}");
             }
-
+            \// Display table in reverse usin a while loop
             Console.WriteLine("\nReversed Multiplication Table:");
             int reverse = 10;
+
+            // loop that generates multiplication table in reverse
+            while (reverse > 0)
+            {
+                // Displays multiplication table in reverse
+                Console.WriteLine($"{reverse} x {num} = {reverse * num}");
+
+                // decrement reverse
+                reverse--;
+            }
+
+            Console.WriteLine("\nFib");
         }
     }
 }
